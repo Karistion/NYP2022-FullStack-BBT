082458222cfef3941ff4beacacb4eea24d23c79a
ryt587 <ryanteoxuebin@gmail.com>
2022-06-24 15:39:25 +0800

    .

--- routes/forums/forums.js
+++ routes/forums/forums.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const res = require('express/lib/response');
 const router = express.Router();
 router.get('/forums', (req, res) => {
-	res.render('forums\customer\homeforums', {layout: 'main'});
+	res.render('forums/customer/homeforums', {layout: 'main'});
 });
 
 module.exports = router;
