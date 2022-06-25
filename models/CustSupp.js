0559e06cbd2bb9bf3a3da668986c74ff96854544
ax4mineshaft <tristanvipero@gmail.com>
2022-06-25 20:41:28 +0800

    .

--- models/CustSupp.js
+++ models/CustSupp.js
@@ -3,11 +3,11 @@ const db = require('../config/DBConfig');
 // Create users table in MySQL Database
 const Threads = db.define('Threads',
 {
-    thread_ID:{ type: Sequelize.INTEGER },
+    
     thread_Title:{ type: Sequelize.STRING },
     thread_Content:{ type: Sequelize.STRING },
-    username:{ type: Sequelize.STRING },
-
+    //thread_Username:{ type: Sequelize.STRING },
+    //dateCreated:{ type: Sequelize.DATE },
 
 
 });
--- routes/forums/forums.js
+++ routes/forums/forums.js
@@ -1,8 +1,44 @@
 const express = require('express');
 const res = require('express/lib/response');
+const Threads = require('../../models/CustSupp');
 const router = express.Router();
+const moment = require('moment');
+
 router.get('/forums', (req, res) => {
-	res.render('forums/customer/homeforums', {layout: 'main'});
+	
+	Threads.findAll({
+        
+        //order: [['dateRelease', 'DESC']],
+        raw: true
+    })
+        .then((threads) => {
+            // pass object to listVideos.handlebar
+            
+            res.render('forums/customer/homeforums', { threads }, {layout: 'main'});
+        })
+        .catch(err => console.log(err));
+});
+
+router.get('/CreateThread', (req, res) => { //this is to render the page
+    res.render('forums/customer/createforums', { layout: 'main' });
+});
+
+router.post('/CreateThread', async function (req, res) { //this is to get the input of the page, req is to get what the user input
+    let thread_Title = req.body.thread_Title;
+    let thread_Content = req.body.thread_Content.slice(0, 1999);
+    let dateCreated = moment(req.body.dateRelease, 'DD/MM/YYYY');
+
+    
+    Threads.create(
+        {
+            thread_Title, thread_Content, dateCreated
+        }
+    )
+        .then((Thread) => {
+            console.log(Thread.toJSON());
+            res.redirect('/');
+        })
+        .catch(err => console.log(err))
 });
 
 module.exports = router;
