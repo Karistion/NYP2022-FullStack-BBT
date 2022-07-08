35bea3e8340a2a77c3d0a0db7054dcd9d205cbe2
ryt587 <ryanteoxuebin@gmail.com>
2022-07-08 21:14:25 +0800

    .

--- models/Drink.js
+++ models/Drink.js
@@ -5,6 +5,6 @@ const Drink = db.define('drinks',
         name: { type: Sequelize.STRING },
         price: { type: Sequelize.DECIMAL(10,2) },
         category: { type: Sequelize.STRING },
-        desc: { type: Sequelize.STRING }
+        desc: { type: Sequelize.STRING }, 
     });
 module.exports = Drink
