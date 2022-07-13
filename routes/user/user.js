f086cdb1cdd4509463788dd153d1c57fac713f92
JordanGoh4 <lowonmath@gmail.com>
2022-07-13 23:28:07 +0800

    testing practical 10

--- routes/user/user.js
+++ routes/user/user.js
@@ -8,8 +8,11 @@ const passport = require('passport');
 const ensureAuthenticated = require('../../helpers/auth');
 const uuid = require('uuid');
 const Cart = require('../../models/Cart');
+// Required for email verification
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const jwt_secret = 'secret';
+const sgMail = require('@sendgrid/mail');
+
 
 router.get('/login', (req, res) => { //this is where we get the info
     res.render('user/customer/login', { layout: 'main' }); //this is for the handlebar name
@@ -18,6 +21,23 @@ router.get('/login', (req, res) => { //this is where we get the info
 router.get('/register', (req, res) => { //this is to render the page
     res.render('user/customer/register', { layout: 'main' });
 });
+
+function sendEmail(toEmail, url) {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const message = {
+        to: toEmail,
+        from: `Video Jotter <${process.env.SENDGRID_SENDER_EMAIL}>`,
+        subject: 'Verify Video Jotter Account',
+        html: `Thank you registering with Video Jotter.<br><br> Please
+<a href=\"${url}"><strong>verify</strong></a> your account.`
+    };
+    // Returns the promise from SendGrid to the calling function
+    return new Promise((resolve, reject) => {
+        sgMail.send(message)
+            .then(response => resolve(response))
+            .catch(err => reject(err));
+    });
+}
 
 router.post('/register', async function (req, res) { //this is to get the input of the page, req is to get what the user input
     let { name, email, password, password2, mobile, postal, address, username, gender } = req.body; //password/password2 is from the input name in the html
@@ -53,6 +73,21 @@ router.post('/register', async function (req, res) { //this is to get the input 
             var hash = bcrypt.hashSync(password, salt);
             // Use hashed password
             let user = await User.create({ name, email, gender, password: hash, mobile, postal, address, username });
+            // Send email
+            let token = jwt.sign(email, process.env.APP_SECRET);
+            let url = `${process.env.BASE_URL}:${process.env.PORT}/user/verify/${user.id}/${token}`;
+            sendEmail(user.email, url)
+                .then(response => {
+                    console.log(response);
+                    flashMessage(res, 'success', user.email + ' registered successfully');
+                    res.redirect('/user/login');
+                })
+                .catch(err => {
+                    console.log(err);
+                    flashMessage(res, 'error', 'Error when sending email to ' +
+                        user.email);
+                    res.redirect('/');
+                });
             var userId=user.id;
             let cart = await Cart.create({ userId })
             // for deafault value is it role:member?
@@ -172,6 +207,42 @@ router.post('/forgotpassword', async function (req,res){
     {
         flashMessage(res, 'error', username + ' does not exist');
         res.render('user/customer/forgotpassword', { layout: 'main' });
+    }
+});
+
+router.get('/verify/:userId/:token', async function (req, res) {
+    let id = req.params.userId;
+    let token = req.params.token;
+    try {
+        // Check if user is found
+        let user = await User.findByPk(id);
+        if (!user) {
+            flashMessage(res, 'error', 'User not found');
+            res.redirect('/user/login');
+            return;
+        }
+        // Check if user has been verified
+        if (user.verified) {
+            flashMessage(res, 'info', 'User already verified');
+            res.redirect('/user/login');
+            return;
+        }
+        // Verify JWT token sent via URL
+        let authData = jwt.verify(token, process.env.APP_SECRET);
+        if (authData != user.email) {
+            flashMessage(res, 'error', 'Unauthorised Access');
+            res.redirect('/user/login');
+            return;
+        }
+        let result = await User.update(
+            { verified: 1 },
+            { where: { id: user.id } });
+        console.log(result[0] + ' user updated');
+        flashMessage(res, 'success', user.email + ' verified. Please login');
+        res.redirect('/user/login');
+    }
+    catch (err) {
+        console.log(err);
     }
 });
 module.exports = router;
