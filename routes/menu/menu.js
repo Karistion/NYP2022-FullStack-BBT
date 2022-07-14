2dfd86320b6014329f4b23a8ffc119ddab9d7b9c
Karistion <97244619+Karistion@users.noreply.github.com>
2022-07-14 15:00:43 +0800

    Update menu.js

--- routes/menu/menu.js
+++ routes/menu/menu.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const res = require('express/lib/response');
 const router = express.Router();
+const flashMessage = require('../../helpers/messenger');
 const Drink = require('../../models/Drink');
 
 router.get('/menu', (req, res) => {
 	res.render('menu/customer/menu', {layout: 'main'});
+});
+
+router.get('/category/:category', (req, res) => {
+	var category=req.params.category;
+	res.render('menu/customer/category', {layout: 'main'});
 });
 
 module.exports = router;
