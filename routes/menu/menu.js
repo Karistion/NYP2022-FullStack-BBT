75fa75be933755586de0f79c52598f2787b17dc3
Karistion <97244619+Karistion@users.noreply.github.com>
2022-08-01 21:36:49 +0800

    ccccc

--- routes/menu/menu.js
+++ routes/menu/menu.js
@@ -5,7 +5,7 @@ const flashMessage = require('../../helpers/messenger');
 const Drink = require('../../models/Drink');
 const ensureAuthenticated = require('../../helpers/auth');
 
-
+// CUSTOMER SIDE
 router.get('/menu', (req, res) => {
 	res.render('menu/customer/menu', {layout: 'main'});
 });
@@ -16,11 +16,17 @@ router.get('/category/:category', async (req, res) => {
 	res.render('menu/customer/category', {layout: 'main', drink});
 });
 
+// ADMIN SIDE
 router.get('/adminmenu', ensureAuthenticated, async (req, res) => {
-	var page='addition';
+	var page='menuchange';
 	var category=req.params.category;
 	var drink = await Drink.findAll({order: [['category']], raw: true});
 	res.render('menu/admin/drink', {layout: 'admin', page, drink});
 });
+
+router.get('/AddDrinks', ensureAuthenticated, async (req, res) => { 
+    res.render('menu/admin/add_drinks', { layout: 'admin' });
+});
+
 
 module.exports = router;
