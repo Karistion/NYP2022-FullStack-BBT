d9e582a20b6a669bfbf62da56d7dd7bebb65503b
Karistion <97244619+Karistion@users.noreply.github.com>
2022-08-01 20:12:47 +0800

    menujs

--- routes/menu/menu.js
+++ routes/menu/menu.js
@@ -3,6 +3,8 @@ const res = require('express/lib/response');
 const router = express.Router();
 const flashMessage = require('../../helpers/messenger');
 const Drink = require('../../models/Drink');
+const ensureAuthenticated = require('../../helpers/auth');
+
 
 router.get('/menu', (req, res) => {
 	res.render('menu/customer/menu', {layout: 'main'});
@@ -12,6 +14,10 @@ router.get('/category/:category', async (req, res) => {
 	var category=req.params.category;
 	var drink = await Drink.findAll({where: {category: category}, order: [['name']], raw: true});
 	res.render('menu/customer/category', {layout: 'main', drink});
+});
+
+router.get('/adminmenu', ensureAuthenticated, async (req, res) => {
+	res.render('menu/admin/add_product', {layout: 'admin', drink});
 });
 
 module.exports = router;
