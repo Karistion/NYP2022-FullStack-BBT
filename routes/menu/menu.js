cda29962a47b9a40e57429dd75867646214d503d
Karistion <97244619+Karistion@users.noreply.github.com>
2022-08-15 10:40:46 +0800

    clarity
    for myself

--- routes/menu/menu.js
+++ routes/menu/menu.js
@@ -9,10 +9,12 @@ const Cartitems = require('../../models/CartItems');
 const ensureAuthenticated = require('../../helpers/auth');
 
 // CUSTOMER SIDE
+
+//menu
 router.get('/menu', (req, res) => {
 	res.render('menu/customer/menu', {layout: 'main'});
 });
-
+//menu category
 router.get('/category/:category', async (req, res) => {
 	var category=req.params.category;
 	var drink = await Drink.findAll({where: {category: category}, order: [['name']], raw: true});
@@ -20,24 +22,26 @@ router.get('/category/:category', async (req, res) => {
 });
 
 // ADMIN SIDE
+
+//admin drink list
 router.get('/adminmenu', ensureAuthenticated, async (req, res) => {
 	var page='menuchange';
 	var category=req.params.category;
 	var drink = await Drink.findAll({order: [['category']], raw: true});
 	res.render('menu/admin/drink_list', {layout: 'admin', page, drink});
 });
-
+//admin drink options
 router.get('/adminmenu2', ensureAuthenticated, async (req, res) => {
 	var page='optionchange';
 	var topping=req.params.topping;
 	var CartItems = await Cartitems.findAll({order: [['topping']], raw: true});
 	res.render('menu/admin/drink_options', {layout: 'admin', page, CartItems});
 });
-
+//admin add drinks from drink list
 router.get('/AddDrinks', ensureAuthenticated, async (req, res) => { 
     res.render('menu/admin/add_drinks', { layout: 'admin' });
 });
-
+//admin add drinks from drink list function
 router.post('/AddDrinks', ensureAuthenticated, async (req, res) => { 
     var name = req.body.name;
 	var price = req.body.price;
@@ -56,12 +60,12 @@ router.post('/AddDrinks', ensureAuthenticated, async (req, res) => {
         })
         .catch(err => console.log(err))
 });
-
+//admin edit drinks from drink list
 router.get('/EditDrinks/:id',ensureAuthenticated, async (req, res) => { //this is to render the page
     var drink = await Drink.findByPk(req.params.id);
 	res.render('menu/admin/edit_drinks', { layout: 'admin', drink});
 });
-
+//admin edit drinks from drink list function
 router.post('/EditDrinks/:id', ensureAuthenticated, async (req, res) => {
 	var name = req.body.name;
 	var price = req.body.price;
@@ -80,7 +84,7 @@ router.post('/EditDrinks/:id', ensureAuthenticated, async (req, res) => {
         })
         .catch(err => console.log(err));
 });
-
+//admin delete drinks from drink list inside drink list page
 router.get('/DeleteDrinks/:id', ensureAuthenticated, async function(req, res) {
 	try {
 		
@@ -91,4 +95,6 @@ router.get('/DeleteDrinks/:id', ensureAuthenticated, async function(req, res) {
 			console.log(err);	
 		}
 });
+
+
 module.exports = router;
